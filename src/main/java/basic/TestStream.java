64a740a6a0eb235a4d0e6419c5e86ee120e86665
JKme <2935865+JKme@users.noreply.github.com>
2022-05-20 18:29:46 +0800

    Default Changelist

---
+++ src/main/java/basic/TestStream.java
@@ -0,0 +1,24 @@
+package basic;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
+
+public class TestStream {
+
+    public static void main(String[] args) throws IOException {
+        TestStream testStream = new TestStream();
+        java.io.ByteArrayOutputStream arrOut = new java.io.ByteArrayOutputStream();
+        ByteArrayOutputStream outputStream = (ByteArrayOutputStream)arrOut;
+
+        ByteArrayOutputStream temOut = outputStream == null ? new ByteArrayOutputStream() : outputStream;
+        GZIPOutputStream gzipOutputStream = new GZIPOutputStream(temOut);
+        String text = "Hello,";
+        byte[] myvar = "Any String you want".getBytes();
+        gzipOutputStream.write(myvar);
+        System.out.println(arrOut);
+//        outputStream = TestStream.base64Encode(text);
+    }
+
+
+}
